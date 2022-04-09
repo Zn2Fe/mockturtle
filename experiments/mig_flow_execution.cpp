46d0fe6a16597ae72b79de9e9bd27f4336b8ed7a
Nicolas Devaux <nicolas.devaux-dias@sfr.fr>
2022-04-09 14:35:39 +0200

    add rep config to path

--- experiments/mig_flow_execution.cpp
+++ experiments/mig_flow_execution.cpp
@@ -26,7 +26,7 @@ int main( int argc, char* argv[] )
   experiment<std::string, std::string, uint32_t, uint32_t, uint32_t, uint32_t, float, bool> exp(
       "mapper", "benchmark", "flow", "size", "size_mig", "depth", "depth_mig", "runtime1", "equivalent" );
   std::string path =  ( argc > 3 ) ? argv[3] : "{}"; /*/home/yuna/Documents/mig_flow_result/*/
-  std::string conf = fmt::format( path, ( argc > 1 ) ? argv[1] : "config.json" );
+  std::string conf = fmt::format( path, ( argc > 1 ) ? fmt::format("config/{}",argv[1]) : "config/config.json" );
   std::cout << "From : " << conf << std::endl;
     
   fmt::print( "[i] processing technology library\n" );
